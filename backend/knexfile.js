ebe44f0594a4486b433db83c8acdb4278bef8baa
Murilo Dutra <murilodut@gmail.com>
2023-05-22 15:17:07 -0300

    Set up docker and docker-compose

---
+++ backend/knexfile.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+module.exports = {
+    [process.env.NODE_ENV]: {
+        client: "pg",
+        connection: {
+            host: process.env.POSTGRES_HOST,
+            port: 5432,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+        },
+        migrations: {
+            directory: "./migrations",
+        },
+    },
+};
