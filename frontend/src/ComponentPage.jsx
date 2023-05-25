fde74d15c2bddd5936f9dcbc559c542f9278281f
Murilo Dutra <murilodut@gmail.com>
2023-05-25 09:42:59 -0300

    Implement "delete component" button

--- frontend/src/ComponentPage.jsx
+++ frontend/src/ComponentPage.jsx
@@ -4,67 +4,90 @@ import { Grid, Button, Box, Toolbar, Divider, Typography, Select, TextField, Men
 import { DataGrid } from '@mui/x-data-grid';
 import AuthContext from './auth-context'
 
-const columns = [
-  {
-    field: 'editar',
-    headerName: 'Editar',
-    width: 90,
-    renderCell: ({ row }) =>
-      <Button
-        onClick={() => console.log(row.id)}
-      >
-        Editar
-      </Button>
-  },
-  {
-    field: 'remover',
-    headerName: 'Remover',
-    width: 90,
-    renderCell: ({ row }) =>
-      <Button
-        onClick={() => console.log(row.id)}
-      >
-        Remover
-      </Button>
-  },
-  { field: 'nome', headerName: 'Nome', width: 130 },
-  { field: 'gtin', headerName: 'GTIN', width: 130 },
-  { field: 'segmento', headerName: 'Segmento', width: 80 },
-  {
-    field: 'altura',
-    headerName: 'Altura',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'largura',
-    headerName: 'Largura',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'profundidade',
-    headerName: 'Profundidade',
-    type: 'number',
-    width: 100,
-  },
-  {
-    field: 'peso_liquido',
-    headerName: 'Peso líquido',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'peso_bruto',
-    headerName: 'Peso bruto',
-    type: 'number',
-    width: 90,
-  },
-];
+const staticColumns = [
+    { field: 'nome', headerName: 'Nome', width: 130 },
+    { field: 'gtin', headerName: 'GTIN', width: 130 },
+    { field: 'segmento', headerName: 'Segmento', width: 80 },
+    {
+      field: 'altura',
+      headerName: 'Altura',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'largura',
+      headerName: 'Largura',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'profundidade',
+      headerName: 'Profundidade',
+      type: 'number',
+      width: 100,
+    },
+    {
+      field: 'peso_liquido',
+      headerName: 'Peso líquido',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'peso_bruto',
+      headerName: 'Peso bruto',
+      type: 'number',
+      width: 90,
+    },
+]
 
 function DataTable() {
   let auth = useContext(AuthContext);
   const [data,setData] = useState([]);
+  const [refreshFlag, setRefreshFlag] = useState(false);
+
+  const columns = [
+    {
+      field: 'editar',
+      headerName: 'Editar',
+      width: 90,
+      renderCell: ({ row }) =>
+        <Button
+          onClick={() => console.log(row.id)}
+        >
+          Editar
+        </Button>,
+    },
+    {
+      field: 'remover',
+      headerName: 'Remover',
+      width: 90,
+      renderCell: ({ row }) => (
+        <Button
+          onClick={async () => {
+            await deleteComponent(row.id)
+            // force refreshing the table
+            setRefreshFlag((prevFlag) => !prevFlag);
+          }}
+        >
+          Remover
+        </Button>
+      ),
+    },
+    ...staticColumns,
+  ];
+
+  const deleteComponent = async (rowId) => {
+      try {
+        await fetch(`http://localhost:5000/componentes/${rowId}`, {
+          method: 'DELETE',
+          headers: {
+            'Authorization': `Bearer ${auth.user.token}`,
+          },
+        });
+      } catch (error) {
+        console.error('Error occurred:', error);
+      }
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -85,7 +108,7 @@ function DataTable() {
     }
 
       fetchData()
-  }, [auth.user])
+  }, [auth.user, refreshFlag])
 
   return (
     <div style={{ minHeight: 400, width: '100%' }}>
